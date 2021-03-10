3d2800d7934e3046f176b4bce18daeb0cd6f4e91
Onyekachi Godswill <omenukooyekachi690@gmail.com>
2021-03-10 17:07:45 +0100

    added tour

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import TourApp from './Tours/TourApp';
+import ScrollToTop from './ScrollTop';
 
 function App() {
   return (
-    <div className="App">
-      
+    <>
+      <ScrollToTop/>
         <TourApp/>
-    </div>
+    </>
   );
 }
 
